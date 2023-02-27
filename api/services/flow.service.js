4da9538c78618036a3c91e8ed856af2a3488d7ea
lanford33 <lanfordcai@outlook.com>
2023-02-27 22:24:12 +0800

    feat: make sure the account is not linked

--- api/services/flow.service.js
+++ api/services/flow.service.js
@@ -925,6 +925,34 @@ class flowService {
       throw createError.NotFound('flow account not found')
     }
 
+    let script = `
+    import ChildAccount from 0x1b655847a90e644a
+
+    pub fun main(address: Address): Bool {
+        let tagCap = getAuthAccount(address)
+            .getCapability<&ChildAccount.ChildAccountTag>(ChildAccount.ChildAccountTagPrivatePath)
+            .borrow()
+
+        if let cap = tagCap {
+            if let parent = cap.parentAddress {
+                return true
+            }
+        }
+
+        return false
+    }
+    `
+    const isLinked = await fcl.query({
+      cadence: script,
+      args: (arg, t) => [
+        arg(user.flowAccount.address, t.Address)
+      ]
+    })
+
+    if (isLinked) {
+      throw createError.UnprocessableEntity("Account is already linked")
+    }
+
     try {
       const accountInfo = await fcl.send([fcl.getAccount(fcl.sansPrefix(parentAddress))])
     } catch (e) {
